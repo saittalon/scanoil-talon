e1ff73c9459df767c7e591302e862d55cfe2b307
saittalon <novikovand01@gmail.com>
2026-03-08 19:59:05 +0500

    Update models.py

--- models.py
+++ models.py
@@ -176,7 +176,7 @@ class AGZS(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password: str) -> bool:
-        return check_password_hash(password, self.password_hash)
+        return check_password_hash(self.password_hash, password)
 
 
 class WebAppToken(db.Model):
